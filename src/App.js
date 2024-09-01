7b14dcd53dcf56e8f35d352a5c1f2a951f673781
DrakBlack69 <tybyers21@gmail.com>
2024-09-01 00:03:53 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './categories.styles.scss'
+import CategoryItem from './components/category-item/category-item.component';
+import { categories } from './components/directory.component';
+
+const App = () => {
+
+  return (
+    <div className="categories-container">
+      
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category}/>
+      ))}
+      
+    </div>
+  );
+}
+
+export default App;
